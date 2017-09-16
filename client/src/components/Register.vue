4ffcd4d17f6e13297940ffa60e91f99c1d6bffc0
Kari Bancroft <kariabancroft@gmail.com>
2017-09-16 11:22:27 -0700

    use the vuetify styles in our registration page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -4,26 +4,34 @@
 3. styles scoped to this component -->
 
 <template>
-  <div>
-    <h1>Cool! Register!</h1>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email" />
+          <br>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password" />
+          <br>
+          <div class="error" v-html="error"/>
+          <br>
+          <v-btn class="cyan"
+            @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-    <br>
-    <div class="error" v-html="error"/>
-    <br>
-    <button
-      @click="register">Register</button>
-  </div>
 </template>
 
 <!-- This is the controller -->
