706e555b6cec0dd62cb4f29fc093a76d03778b30
Kari Bancroft <kariabancroft@gmail.com>
2017-09-15 17:11:57 -0700

    add a really basic initial route

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,22 @@
-console.log('Hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+// Builds a basic express server
+const app = express()
+
+// logs
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// Give it the HTTP verb you want it to use
+// Then the endpoint
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
