9727a0341654a1cfddd874650f3b41715df1e066
Kari Bancroft <kariabancroft@gmail.com>
2017-09-15 17:51:13 -0700

    add a very simple registration form/component which will communicate with the back end endpoint

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,45 @@
 <template>
   <div>
     <h1>Cool! Register!</h1>
+
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email" />
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password" />
+    <br>
+    <button
+      @click="register">Register</button>
   </div>
 </template>
 
+<!-- This is the controller -->
+<!-- Can bind to attributes -->
 <script>
+import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
+    // two way binding
     return {
-
+      email: 'abc',
+      password: '123'
+    }
+  },
+  methods: {
+    // async & register used instead of promises
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      // console.log('register button was clicked', this.email, this.password)
+      console.log(response.data)
     }
   }
 }
